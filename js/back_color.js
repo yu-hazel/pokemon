cc3bcb7ee78c9867e8cf9e1f141fbe42320c4d83
yoonji <kyjkbw@gmail.com>
2024-07-11 12:09:10 +0900

    카드 백그라운드 색상 객체 js 모듈 분리(다크, 기본), 로딩 로티파일 스크립트 추가

---
+++ js/back_color.js
@@ -0,0 +1,43 @@
+// 필터링 백그라운드 색상 기본+다크모드
+
+export const typeColors = {
+  normal: '#E7E7E7',
+  fighting: '#F2B6B6',
+  poison: '#E7D0F0',
+  ground: '#F2D7AE',
+  flying: '#D0E3FF',
+  bug: '#E6F2CE',
+  rock: '#F2E0BB',
+  water: '#D0E3FF',
+  steel: '#E2E2F2',
+  ghost: '#E0D0E7',
+  fire: '#F2C6A2',
+  electric: '#F2E3C1',
+  grass: '#D6F2C1',
+  ice: '#C1F2F2',
+  psychic: '#F2C1D3',
+  dragon: '#E3D0F2',
+  dark: '#D0D0D0',
+  fairy: '#F2E1F2'
+};
+
+export const darkTypeColors = {
+  normal: '#A1A1A1',
+  fighting: '#B26767',
+  poison: '#B39BBF',
+  ground: '#BFA17B',
+  flying: '#9CB2DF',
+  bug: '#B4C39C',
+  rock: '#BFAE85',
+  water: '#9CB2DF',
+  steel: '#A0A0B0',
+  ghost: '#A396B0',
+  fire: '#BF8A71',
+  electric: '#BFA982',
+  grass: '#A5BFA0',
+  ice: '#84C0C0',
+  psychic: '#BF8A97',
+  dragon: '#B8A0BF',
+  dark: '#909090',
+  fairy: '#BF9FBF'
+};
--- js/main_pokeapi.js
+++ js/main_pokeapi.js
@@ -1,64 +1,24 @@
+import { typeColors, darkTypeColors } from '/pokemon/js/back_color.js';
+
 let offset = 0; // 시작 오프셋
-const limit = 100; // 한번에 가져올 포켓몬 수
+const limit = 20; // 한번에 가져올 포켓몬 수
 const loadedPokemonNames = new Set(); // 이미 로드된 포켓몬 이름을 저장하기 위한 집합
 const cardSec = document.querySelector('.cardSec');
 let isLoading = false; // 중복 요청 방지
 let currentFilter = ''; // 현재 필터된 타입 저장
 
-const typeColors = {
-  normal: '#E7E7E7',
-  fighting: '#F2B6B6',
-  poison: '#E7D0F0',
-  ground: '#F2D7AE',
-  flying: '#D0E3FF',
-  bug: '#E6F2CE',
-  rock: '#F2E0BB',
-  water: '#D0E3FF',
-  steel: '#E2E2F2',
-  ghost: '#E0D0E7',
-  fire: '#F2C6A2',
-  electric: '#F2E3C1',
-  grass: '#D6F2C1',
-  ice: '#C1F2F2',
-  psychic: '#F2C1D3',
-  dragon: '#E3D0F2',
-  dark: '#D0D0D0',
-  fairy: '#F2E1F2'
-};
+// 로딩 화면 요소 선택
+const loadingScreen = document.getElementById('loadingScreen');
 
-const darkTypeColors = {
-  normal: '#A1A1A1',
-  fighting: '#B26767',
-  poison: '#B39BBF',
-  ground: '#BFA17B',
-  flying: '#9CB2DF',
-  bug: '#B4C39C',
-  rock: '#BFAE85',
-  water: '#9CB2DF',
-  steel: '#A0A0B0',
-  ghost: '#A396B0',
-  fire: '#BF8A71',
-  electric: '#BFA982',
-  grass: '#A5BFA0',
-  ice: '#84C0C0',
-  psychic: '#BF8A97',
-  dragon: '#B8A0BF',
-  dark: '#909090',
-  fairy: '#BF9FBF'
-};
-
+// 포켓몬 데이터 가져오기
 async function fetchPokemons(offset, limit, filterType = '') {
   if (filterType) {
     const typeResponse = await fetch(`https://pokeapi.co/api/v2/type/${filterType}`);
     const typeData = await typeResponse.json();
-    const sortedPokemon = typeData.pokemon
-      .map(p => p.pokemon)
-      .sort((a, b) => {
-        const aId = parseInt(a.url.split('/').slice(-2, -1)[0]);
-        const bId = parseInt(b.url.split('/').slice(-2, -1)[0]);
-        return aId - bId;
-      });
-    return sortedPokemon.slice(offset, offset + limit);
+    // console.log('전처리 전:', typeData.pokemon); // 전처리 전 데이터 출력
+    const mappedData = typeData.pokemon.map(p => p.pokemon);
+    // console.log('전처리 후:', mappedData); // 콘솔에 변환된 데이터 출력
+    return mappedData.slice(offset, offset + limit); // 오프셋 적용하여 데이터 나누어 가져오기
   } else {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     const response = await fetch(url);
@@ -67,41 +27,33 @@ async function fetchPokemons(offset, limit, filterType = '') {
   }
 }
 
+// 포켓몬 상세 정보 가져오기
 async function fetchPokemonDetails(pokemon) {
   const response = await fetch(pokemon.url);
   const data = await response.json();
   return data;
 }
 
+// 포켓몬 한글 이름 가져오기
 async function fetchPokemonSpecies(pokemon) {
   const response = await fetch(pokemon.species.url);
   const data = await response.json();
   return data.names.find(name => name.language.name === 'ko').name;
 }
 
-async function fetchPokemonSprite(pokemon) {
-  const response = await fetch(pokemon.url);
-  const data = await response.json();
-  return data.sprites.front_default; // 기본 이미지 경로
-}
-
+// 포켓몬 데이터 로드하고 화면에 표시
 async function loadPokemons() {
   if (isLoading) return; // 이미 로드 중이면 중복 요청 방지
   isLoading = true; // 로딩 시작
+  loadingScreen.style.display = 'flex'; // 로딩 화면 표시
+  document.body.style.overflow = 'hidden'; // 스크롤 막기
   const pokemons = await fetchPokemons(offset, limit, currentFilter);
   for (let pokemon of pokemons) {
     if (!loadedPokemonNames.has(pokemon.name)) { // 중복 확인
       try {
         const details = await fetchPokemonDetails(pokemon);
         const koreanName = await fetchPokemonSpecies(details);
-        let spriteUrl = details.sprites.versions['generation-v']['black-white'].animated.front_default;
-
-        if (!spriteUrl) {
-          spriteUrl = details.sprites.front_default; // 기본 이미지
-          if (!spriteUrl) {
-            spriteUrl = await fetchPokemonSprite(pokemon); // 기본 이미지가 없을 때
-          }
-        }
+        const spriteUrl = details.sprites.front_default; // 기본 이미지 경로
 
         const card = document.createElement('div');
         card.className = 'cardOne';
@@ -133,6 +85,8 @@ async function loadPokemons() {
   }
   offset += limit; // 다음 오프셋 설정
   isLoading = false; // 로딩 종료
+  loadingScreen.style.display = 'none'; // 로딩 화면 숨기기
+  document.body.style.overflow = 'auto'; // 스크롤 다시 활성화
 }
 
 // 필터링 기능
@@ -144,7 +98,7 @@ function filterByType(type) {
   loadPokemons(); // 새로운 데이터 로드
 }
 
-// 다크 모드 토글 기능
+// 카드의 배경색 다크 모드
 function setCardBackgroundColor(card) {
   const type = currentFilter || card.dataset.types.split(' ')[0];
   if (document.body.classList.contains('darkBtn')) {
@@ -153,7 +107,7 @@ function setCardBackgroundColor(card) {
     card.style.backgroundColor = typeColors[type];
   }
 }
-
+// 다크 모드 토글
 function toggleDarkMode() {
   document.body.classList.toggle('darkBtn');
   document.getElementById('lightDarkToggle').classList.toggle('darkBtn');
@@ -164,7 +118,7 @@ function toggleDarkMode() {
 
 // 무한 스크롤 기능
 window.addEventListener('scroll', () => {
-  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 100) {
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight - 200) {
     // 스크롤이 거의 끝에 도달했을 때 추가 포켓몬 로드
     loadPokemons();
   }
