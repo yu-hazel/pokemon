22805950305e12f7e7028f385a008f63514040f9
yoonji <kyjkbw@gmail.com>
2024-07-18 10:06:03 +0900

    💄 style: [Vue] 메인 피드백문구 추가

--- pokemon_vue/src/components/pokeHeader.vue
+++ pokemon_vue/src/components/pokeHeader.vue
@@ -8,11 +8,11 @@
           <router-link to=/minigame> 미니게임</router-link>
         </div>
         <div class="userMenu">
-          <p class="langText">
+          <p class="langText" @mouseover="showUpdateNotice" @mouseout="hideUpdateNotice">
             <img src="@/assets/img//lang.png" alt id="lang" />한국어
           </p>
-          <span class="updateNotice">현재는 한국어만 지원됩니다. 추후 일본어 영어 지원 예정!</span>
-          <p><img src="@/assets/img//login.png" alt id="login" />로그인</p>
+          <span class="updateNotice" v-show="isUpdateNoticeVisible">현재는 한국어만 지원됩니다. 추후 일본어 영어 지원 예정!</span>
+          <p class="login"><img src="@/assets/img//login.png" alt id="login" />로그인</p>
         </div>
         <img id="monsterBall" src="@/assets/img//monster_ball.png" alt @click.stop="toggleMenu" />
       </div>
@@ -22,9 +22,23 @@
   </div>
 </template>
 <script setup>
+  import { ref } from "vue";
+
 const isMenuOn = ref(false);
 // 메뉴를 토글하는 함수
 const toggleMenu = () => {
   isMenuOn.value = !isMenuOn.value;
 };
+
+//한국어 지원 피드백문구
+ const isUpdateNoticeVisible = ref(false);
+
+    const showUpdateNotice = () => {
+      isUpdateNoticeVisible.value = true;
+    };
+
+    const hideUpdateNotice = () => {
+      isUpdateNoticeVisible.value = false;
+    };
+
 </script>
