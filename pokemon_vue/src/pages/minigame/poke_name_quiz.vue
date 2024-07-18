84782afa5de64028c4fd06a0e6aa0d503bbfc442
yoonji <kyjkbw@gmail.com>
2024-07-18 10:18:03 +0900

    이름 퀴즈 파일 생성

---
+++ pokemon_vue/src/pages/minigame/poke_name_quiz.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">게임 준비 중입니다...🥲🙏</div>
+</template>
+
+<style scoped>
+@import url("https://webfontworld.github.io/DungGeunMo/DungGeunMo.css");
+/* 둥근모꼴 */
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "DungGeunMo";
+}
+.test {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  font-size: 25px;
+  color: black;
+}
+</style>
