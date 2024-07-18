3973a78dfb59f7b0df3846a93fafa82abe7004a0
yoonji <kyjkbw@gmail.com>
2024-07-18 10:35:32 +0900

    💄 style: [Vue] 푸터

--- pokemon_vue/src/components/pokeFooter.vue
+++ pokemon_vue/src/components/pokeFooter.vue
@@ -1,3 +1,43 @@
 <template>
-<h2>푸터예요</h2>
+  <footer>
+    <a href="https://github.com/yoo-nji">
+      <img src="/src/assets/img/footer_git.png" alt="github" />
+      <span>yoonji</span>
+    </a>
+    <a href="https://github.com/yu-hazel">
+      <img src="/src/assets/img/footer_git.png" alt="github" />
+      <span>hyein</span>
+    </a>
+  </footer>
 </template>
+<style scoped>
+/* 푸터 */
+footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 40px;
+  width: 100%;
+  height: 30px;
+  position: fixed;
+  bottom: 0;
+  background: #fda7a7;
+  color: white;
+}
+footer > a {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  width: 97px;
+  height: 25px;
+  border-radius: 42px;
+  transition: 0.3s;
+}
+footer > a > img {
+  width: 20px;
+}
+footer > a:hover {
+  background: rgba(242, 242, 242, 0.38);
+}
+</style>
