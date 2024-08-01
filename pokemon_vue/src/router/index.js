97831b502e7e14d7d72f5743f7cc3f53024e02c7
yoonji <kyjkbw@gmail.com>
2024-08-02 00:11:19 +0900

    🐛 fix: 라우트 파일 수정

--- pokemon_vue/src/router/index.js
+++ pokemon_vue/src/router/index.js
@@ -1,14 +1,39 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
+// import { createRouter, createWebHistory } from 'vue-router/auto';
 
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto';
+// const router = createRouter({
+//   history: createWebHistory('/build-test/'),
+// });
+
+// export default router;
+
+import { createRouter, createWebHistory } from 'vue-router';
+import Main from '@/pages/Main.vue';
+import MiniGame from '@/pages/MiniGame.vue';
+import PokeCard from '@/pages/poke_card.vue';
+import PokeNameQuiz from '@/pages/poke_name_quiz.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Main,
+  },
+  {
+    path: '/minigame',
+    component: MiniGame,
+  },
+  {
+    path: '/minigame/pokecard',
+    component: PokeCard
+  },
+  {
+    path: '/minigame/namequiz',
+    component: PokeNameQuiz
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory('/pokemon/'),
+  history: createWebHistory('/build-test/'),  // GitHub Pages URL 경로 설정
+  routes,
 });
 
 export default router;
